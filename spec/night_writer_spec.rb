b44019beb76668a8c4ca7001ca0a728d14b29f0e
Amanda Ross <amikaross@gmail.com>
2022-09-22 17:37:43 -0600

    Write tests for #by_lines method and #encode_to_braille method

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -8,6 +8,24 @@ RSpec.describe NightWriter do
     end
   end
 
+  describe "#encode_to_braille" do 
+    it "takes plain text and converts it into braille" do 
+      night_writer = NightWriter.new("./spec/fixtures/single_letter.txt")
+      expect(night_writer.encode_to_braille).to eq("0.\n..\n..")
+    end
+  end
+
+  describe "#by_lines" do 
+    it "returns an array of strings of 40 characters (until final one which may be less)" do 
+      night_writer = NightWriter.new("message.txt")
+      input = "we meet in an hour of change and challenge, in a decade of hope and fear."
+      expected_output = ["we meet in an hour of change and challen",
+                         "ge, in a decade of hope and fear."]
+        
+      expect(night_writer.by_lines(input)).to eq(expected_output)
+    end
+  end
+
   describe "#cli" do 
     it "outputs a string with the correct filename and number of characters" do 
       night_writer = NightWriter.new("./spec/fixtures/test.txt") 
