8fd6b99987587e85edc7c9ba470c080e4dbb9b94
Amanda Ross <amikaross@gmail.com>
2022-09-23 06:34:03 -0600

    Update encode_to_braille to use encode_line from Encoder

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -12,18 +12,7 @@ class NightWriter
 
   def encode_to_braille
     # array_of_lines = by_lines(@message.downcase)
-    braille_chars = @message.split("").each_with_object([]) do |char, array|
-      array << Encoder.encode(char)
-    end 
-    line_1 = ""
-    line_2 = ""
-    line_3 = ""
-    braille_chars.each do |char_array|
-      line_1 << char_array[0]
-      line_2 << char_array[1]
-      line_3 << char_array[2]
-    end
-    braille_message = "#{line_1}\n#{line_2}\n#{line_3}"
+    Encoder.encode_line(@message)
   end
 
   def by_lines(string)
