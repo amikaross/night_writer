3435d8d250571fbdc664c42180a1a890f8f6913d
Amanda Ross <amikaross@gmail.com>
2022-09-23 08:06:41 -0600

    Update encoder and night_writer to output braille file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -10,28 +10,29 @@ class NightWriter
     @filename = ARGV[1]
   end
 
+  def by_lines(string)
+    (0..(string.length-1)/40).map { |i| string[i * 40, 40] }
+  end
+
   def encode_to_braille
     by_lines(@message.downcase).each_with_object("") do |line, string|
       string << "#{Encoder.encode_line(line)}\n"
     end
   end
 
-  # def output_file
-  #   FileIO.
-  # end
-
-  def by_lines(string)
-    (0..(string.length-1)/40).map { |i| string[i * 40, 40] }
+  def output_file
+    FileIO.write(filename, encode_to_braille)
   end
 
-  def cli 
+  def terminal_output
+    output_file
     "Created '#{filename}' containing #{message.length} characters"
   end
 end
 
 # runner code that has to be commented out when running test suite 
-# night_writer = NightWriter.new(ARGV[0])
-# puts night_writer.cli
+night_writer = NightWriter.new(ARGV[0])
+puts night_writer.terminal_output
 
 # require 'pry'; binding.pry
 
