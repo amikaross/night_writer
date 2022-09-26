17b41d700a8ed441628c7a3ed66e69c7c2de491c
Amanda Ross <amikaross@gmail.com>
2022-09-26 13:53:39 -0600

    Refactor by_lines method in NightWriter

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -14,22 +14,17 @@ class NightWriter
   def by_lines(string)
     number_of_braille_chars = string.length + string.scan(/[A-Z]/).length
     number_of_lines = number_of_braille_chars / 40 
-    lines = (0..number_of_lines).map { |i| "" }
     line_number = 0
     counter = 0 
-    string.chars.each do |char|
+    string.chars.each_with_object((0..number_of_lines).map { |i| "" }) do |char, array|
       if counter == 40 
         counter = 0
         line_number += 1
       end
-      lines[line_number] << char 
+      array[line_number] << char 
       ("A".."Z").include?(char) ? counter += 2 : counter += 1 
     end
-    lines
   end
-  # def by_lines(string)
-  #   (0..(string.length-1)/40).map { |i| string[i * 40, 40] }
-  # end
 
   def encode_to_braille
     message = FileIO.read(filename).delete("\n") 
