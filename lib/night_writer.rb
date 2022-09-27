ba09efdb38af11fa917b78aedf610518bec4d333
Amanda Ross <amikaross@gmail.com>
2022-09-27 11:45:08 -0600

    Update NightWriter to read numbers

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -7,7 +7,9 @@ class NightWriter < Translator
   end
 
   def total_lines(string)
-    number_of_braille_chars = string.length + string.scan(/[A-Z]/).length
+    total_caps = string.scan(/[A-Z]/).length
+    total_numbers = string.scan(/[0-9]/).length
+    number_of_braille_chars = string.length + total_caps + total_numbers
     number_of_braille_chars / 40 
   end
 
@@ -15,16 +17,22 @@ class NightWriter < Translator
     line_number = 0
     counter = 0 
     string.chars.each_with_object((0..total_lines(string)).map { |i| "" }) do |char, array|
-      if counter == 40 then counter = 0 and line_number += 1 end 
+      if counter >= 40 then counter = 0 and line_number += 1 end 
       array[line_number] << char 
-      ("A".."Z").include?(char) ? counter += 2 : counter += 1 
+      if ("A".."Z").include?(char) || ("0".."9").include?(char)
+        counter += 2 
+      else 
+        counter += 1 
+      end 
     end
   end
 
   def encode_char(char)
     if ("A".."Z").include?(char)
       char = char.downcase
-      ["..", "..", ".0"].zip(self.dictionary[char]).map(&:join)
+      ["..", "..", ".0"].zip(dictionary[char]).map(&:join)
+    elsif ("0".."9").include?(char)
+      [".0", ".0", "00"].zip(dictionary[to_alpha(char)]).map(&:join)
     else 
       braille_char = dictionary[char]
     end
